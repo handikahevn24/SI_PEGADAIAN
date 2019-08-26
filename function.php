b525574dc7db931fb3fcfb457183d4d835f3400b
handikahevn <handikahevn24@gmail.com>
2019-08-26 10:13:54 +0700

    Perbaikan

--- function.php
+++ function.php
@@ -19,17 +19,44 @@ $query = "SELECT * FROM user where username ='$username' and password = '$passwo
 $mysqli = Database::getInstance()->getConnection();
 $result = $mysqli->query($query);
     if($result->num_rows > 0){
-        header("location:admin/controller.php?hal=dashboard");
+        // header("location:admin/controller.php?hal=dashboard");
         session_start();
         $_SESSION['username'] = $username;
+        echo "<script>alert('Login Berhasil.'); window.location = 'admin/nasabah.php'</script>";
     }else{
         header("location:index.php");
     }
 }
 
+// Fungsi Tambah Nasabah
 
-//Kumpulan Func
+function tambahNasabah($noidnasabah, $namanasabah, $jeniskelaminnasabah, $tempatlahirnasabah, $tanggallahirnasabah, $agamanasabah, $pendidikannasabah, $tanggalpendaftaran ){
+    $query = "INSERT INTO nasabah (noidnasabah, namanasabah, jeniskelaminnasabah, tempatlahirnasabah, tanggallahirnasabah, agamanasabah, pendidikannasabah, tanggalpendaftaran) values($noidnasabah, $namanasabah, $jeniskelaminnasabah, $tempatlahirnasabah, $tanggallahirnasabah, $agamanasabah, $pendidikannasabah, $tanggalpendaftaran)" or die(mysql_error());
+}
+
+//Fungsi Tambah Pengajuan
+function tambahPengajuan($idpengajuan, $nopinjaman, $tujuanpengajuan, $besarpengajuan, $jangkawaktupengajuan, $tanggalpengajuan){
+    $query = "INSERT INTO pengajuan_pinjaman (idpengajuan, nopinjaman, tujuanpengajuan, besarpengajuan, jangkawaktupengajuan, tanggalpengajuan) values($idpengajuan, $nopinjaman, $tujuanpengajuan, $besarpengajuan, $jangkawaktupengajuan, $tanggalpengajuan)";
+
+}
+
+//Fungsi Tambah Pinjaman
+function tambahPinjaman($nopinjaman, $besarpinjaman, $barangjaminan, $tanggalpinjaman, $tanggalberakhir){
+    $query = "INSERT INTO pinjaman (nopinjaman, besarpinjaman, barangjaminan, tanggalpinjaman, tanggalberakhir) values($nopinjaman, $besarpinjaman, $barangjaminan, $tanggalpinjaman, $tanggalberakhir)";
+
+}
+
+/**
+ * Kumpulan Func
+ */
+
+//Func Login
 if($func == "login"){
     login($_POST['username'], $_POST['password']);
+}
+
+//Func Tambah Nasabah
+if($func == "tambahNasabah"){
+    tambahNasabah($_POST['noidnasabah'], $_POST['namanasabah'], $_POST['jeniskelaminnasabah'], $_POST['tempatlahirnasabah'], $_POST['tanggallahirnasabah'], $_POST['agamanasabah'], $_POST['pendidikannasabah'], $_POST['tanggalpendaftaran']);
 }
 ?>
